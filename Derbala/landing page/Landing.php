860be845c0f3cbb4079394f05eede03714c19174
David Gibran Maulana <139204443+DavidGibran@users.noreply.github.com>
2024-07-11 00:31:33 +0700

    Add files via upload

---
+++ Derbala/landing page/Landing.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./Style1.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css" rel="stylesheet" />
+    <title>Derbala</title>
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <a href="#" class="logo-nav"><img src="./asset/derbala-logo.svg" alt="nav-logo"></a>
+        <nav class="navbar">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Our Services <i class="ri-arrow-down-s-line"></i></a></li>
+                <li><a href="#">Testimonial</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section>
+            <div class="hero-container">
+                <div class="main-element">
+                    <img src="./asset/elementLP.png" alt="element-utm">
+                </div>
+                <div class="main-text">
+                    <div class="text-header">
+                        <h1>Derbala<span class="com">.com</span> </h1>
+                    </div>
+                    <div class="text-header2">
+                        <h3>Mengukir Jejak Dalam Media Sosial dan Digital Marketing</h3>
+                    </div>
+                    <div class="paragraph">
+                        <p>Kami adalah agen pemasaran digital yang memandu bisnis menuju kesuksesan online. berfokus pada pertumbuhan bisnis, kami membantu klien mencapai visibilitas maksimum, konversi tinggi, dan mengukir jejak yang kuat di dunia digital.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="sec-aboutUs">
+            <div class="aboutUs-container">
+                <div class="header-aboutUs">
+                    <h1>About Us</h1>
+                </div>
+                <div class="text-aboutUs">
+                    <p>
+                        <span class="span-aboutUs">Tap Media Creative Derbala</span>, Digital Marketing Agency yang menjalankan misi untuk menyajikan dan mengukir di setiap jejak dalam karya kami. Eksplorasi strategi media sosial terbaik untuk mengungkapkan tujuan bisnis kamu.
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="services-container">
+            <div class="header-text1">
+                <div class="line"></div>
+                <div class="header-services">
+                    <h1>Our Services</h1>
+                </div>
+                <div class="line"></div>
+            </div>
+            <div class="header-text2">
+                <h1>Layanan Kami</h1>
+            </div>
+            <div class="content">
+                <div class="card-service">
+                    <div class="img-service"><img src="./asset/element_sv1.svg" alt="sv1"></div>
+                    <div class="text-service">
+                        <div class="header-card">
+                            <h3>Website Building</h3>
+                        </div>
+                        <div class="start-from">
+                            <P>Mulai Dari</P>
+                        </div>
+                        <div class="price-sv">
+                            <p>Rp. 1500k </p>
+                        </div>
+                        <p>Berbagai solusi terkait Website Building untuk membantu bisnis kamu.</p>
+                    </div>
+                </div>
+                <div class="card-service">
+                    <div class="img-service"><img src="./asset/element_sv2.svg" alt="sv2"></div>
+                    <div class="text-service">
+                        <div class="header-card">
+                            <h3>Creative Content Studio</h3>
+                        </div>
+                        <div class="start-from">
+                            <P>Mulai Dari</P>
+                        </div>
+                        <div class="price-sv">
+                            <p>Rp. 250k </p>
+                        </div>
+                        <p>Jangan ragu mencapai goals dengan solusi kreatif dan berkualitas tinggi.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="sec-testimonials">
+            <div class="header-text1">
+                <div class="line"></div>
+                <div class="header-services">
+                    <h1>Testimonials</h1>
+                </div>
+                <div class="line"></div>
+            </div>
+            <div class="header-text2">
+                <h1>Apa Yang Mereka Katakan?</h1>
+            </div>
+        </section>
+        <section class="sec-contactUs">
+            <div class="header-text1">
+                <div class="line"></div>
+                <div class="header-services">
+                    <h1>Contact Us</h1>
+                </div>
+                <div class="line"></div>
+            </div>
+            <div class="header-text2">
+                <h1>Hubungi Kami</h1>
+            </div>
+        </section>
+    </main>
+</body>
+
+</html>
